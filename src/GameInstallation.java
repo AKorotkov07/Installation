fbf3e15a8f33286a4ea0ab70e790ae1e5486f8e2
AKorotkov07 <153372700+AKorotkov07@users.noreply.github.com>
2024-02-04 09:13:48 +0300

    Add files via upload

--- src/GameInstallation.java
+++ src/GameInstallation.java
@@ -7,46 +7,59 @@ public class GameInstallation {
         if (!gamesDir.exists())
             gamesDir.mkdirs();
 
-        createDirectory(gamesDir, "src");
-        createDirectory(gamesDir, "res");
-        createDirectory(gamesDir, "savegames");
-        createDirectory(gamesDir, "temp");
+        StringBuilder log = new StringBuilder();
+
+        log.append(createDirectory(gamesDir, "src"));
+        log.append(createDirectory(gamesDir, "res"));
+        log.append(createDirectory(gamesDir, "savegames"));
+        log.append(createDirectory(gamesDir, "temp"));
 
         File srcDir = new File(gamesDir, "src");
-        createDirectory(srcDir, "main");
-        createDirectory(srcDir, "test");
+        log.append(createDirectory(srcDir, "main"));
+        log.append(createDirectory(srcDir, "test"));
 
         File mainDir = new File(srcDir, "main");
-        createFile(mainDir, "Main.java");
-        createFile(mainDir, "Utils.java");
+        log.append(createFile(mainDir, "Main.java"));
+        log.append(createFile(mainDir, "Utils.java"));
 
         File resDir = new File(gamesDir, "res");
-        createDirectory(resDir, "drawables");
-        createDirectory(resDir, "vectors");
-        createDirectory(resDir, "icons");
+        log.append(createDirectory(resDir, "drawables"));
+        log.append(createDirectory(resDir, "vectors"));
+        log.append(createDirectory(resDir, "icons"));
 
         File tempDir = new File(gamesDir, "temp");
         File tempFile = new File(tempDir, "temp.txt");
-        createFile(tempDir, "temp.txt");
+        log.append(createFile(tempDir, "temp.txt"));
 
-        StringBuilder log = new StringBuilder();
-        log.append("Файлы и папки успешно созданы");
         writeLogToFile(tempFile, log.toString());
     }
 
-    private static void createDirectory(File parentDir, String dirName) {
+    private static String createDirectory(File parentDir, String dirName) {
         File dir = new File(parentDir, dirName);
-        if (!dir.exists())
-            dir.mkdir();
+        if (!dir.exists()) {
+            if (dir.mkdir()) {
+                return "Директория " + dirName + " успешно создана\n";
+            } else {
+                return "Ошибка при создании директории " + dirName + "\n";
+            }
+        }
+        return "Директория " + dirName + " уже существует\n";
     }
 
-    private static void createFile(File parentDir, String fileName) {
+    private static String createFile(File parentDir, String fileName) {
         File file = new File(parentDir, fileName);
         try {
-            if (!file.exists())
-                file.createNewFile();
+            if (!file.exists()) {
+                if (file.createNewFile()) {
+                    return "Файл " + fileName + " успешно создан\n";
+                } else {
+                    return "Ошибка при создании файла " + fileName + "\n";
+                }
+            }
+            return "Файл " + fileName + " уже существует\n";
         } catch (IOException e) {
             e.printStackTrace();
+            return "Ошибка при создании файла " + fileName + "\n";
         }
     }
 
